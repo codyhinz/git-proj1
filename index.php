ee3177c3fba010b5a6f0ef17c7522e12d6e8c2e3
Cody Hinz <codyhinz@gmail.com>
2021-02-01 18:52:08 -0500

    Added more styling and date

--- index.php
+++ index.php
@@ -1,6 +1,11 @@
 <!DOCTYPE html>
-<head>
-    <link rel="stylesheet" href="style.css">
-    <title>My first PHP page</title>
-</head>
-<h1>Hello world</h1>
+    <head>
+        <link rel="stylesheet" href="style.css">
+        <title>My first PHP page</title>
+    </head>
+    <body>
+        <header>
+            <?php echo date("d/m/Y") ?>
+        </header>
+        <main>Hello World</main>
+    </body>
